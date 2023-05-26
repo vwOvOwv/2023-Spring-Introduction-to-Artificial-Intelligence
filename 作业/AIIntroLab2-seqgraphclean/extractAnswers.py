01dcbe6bc46a5ba57a4d8b257bcc24eb0e07846e
Memories <1271440868@qq.com>
2023-05-27 00:19:50 +0800

    update

--- 作业/AIIntroLab2-seqgraphclean/extractAnswers.py
+++
@@ -1,4 +0,0 @@
-import zipfile
-f = zipfile.ZipFile("answer.zip", "r", zipfile.ZIP_DEFLATED)
-f.extractall()
-f.close()
--- 作业/lab3-v1/compressAnswers.py
+++
@@ -1,8 +0,0 @@
-import zipfile
-
-filenames = ["info.yaml", "FruitModel.py", 'model/mlp.npy']
-
-f = zipfile.ZipFile("answer.zip", "w", zipfile.ZIP_DEFLATED)
-for filename in filenames:
-    f.write(filename)
-f.close()
